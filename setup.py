1f658b06f572ada4d3ff91af463f6ded6da1fe29
thomas <thomas@localhost.localdomain>
2020-09-21 22:17:24 -0400

    Updating setup

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup, find_namespace_packages
+from distutils.core import setup
+from setuptools import find_namespace_packages
 
 setup(
     name='cosmic17-wire',
+    version='0.1',
+    description='',
+    long_description=open('README.md').read(),
     packages=find_namespace_packages(where='src'),
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    license='GPLv2',
+    url='https://github.com/RandomRedBeard/cosmic17-wire',
+    author='Thomas Jansen',
+    author_email='tnj@17cosmicln.net'
 )
